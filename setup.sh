10c612ba0f4b1479ac6f3b99cab26c4b5a16f64d
Kaushal <kaushal.v.bhat@gmail.com>
2020-09-09 20:13:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-node_modules/.bin/eslint --init
+# node_modules/.bin/eslint --init
 
-export GOOGLE_APPLICATION_CREDENTIALS="/home/k4u5h4l/temp-stuff/discord-test-run/discord-bot-config.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/path/to/discord-bot-config.json"
